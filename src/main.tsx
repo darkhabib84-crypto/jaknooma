fcd6ece8fc95aa9db1274785dbb5f30c5f7d434c
darkhabib84-crypto <dark.habib84@gmail.com>
2026-07-17 02:37:39 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { CartProvider } from './contexts/CartContext';
+// 1. استيراد BrowserRouter هنا
+import { BrowserRouter } from 'react-router-dom'; 
 
 // منع التظليل التلقائي للنصوص على مستوى المتصفح
 document.addEventListener('contextmenu', (event) => event.preventDefault());
@@ -11,8 +13,10 @@ document.onselectstart = () => false;
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <CartProvider>
-      <App />
+      {/* 2. تغليف التطبيق بالكامل بـ BrowserRouter */}
+      <BrowserRouter> 
+        <App />
+      </BrowserRouter>
     </CartProvider>
   </StrictMode>,
 );
-
